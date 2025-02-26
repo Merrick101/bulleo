b2b1f8a6e754b7ab6df810b7f32bae295b5ea545
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 19:36:21 +0000

    Set allauth's authentication backends

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
