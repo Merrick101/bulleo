1a77de812f7da27dd7065de9991e06ad9a109033
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 19:25:36 +0000

    Add basic styling for homepage, navbar, footer, add load_static to base.html, update core url imports and  patterns

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.news.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("", homepage, name="home"),
     path('news/', include('apps.news.urls', namespace="news")),
     path('users/', include('apps.users.urls', namespace="users")),
 ]
