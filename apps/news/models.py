c11e7e323c4d7b1771d8d9b4f6c05207a970c446
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 12:45:51 +0000

    Modify Article model to store external image links in the database

--- apps/news/models.py
+++ apps/news/models.py
@@ -6,4 +6,9 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # Store external image links
+    image_url = models.URLField(max_length=500, blank=True, null=True)
     published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
