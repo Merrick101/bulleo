cfb0285c78ece214b211e3b80405f2fc8615f4b3
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 16:00:59 +0000

    Update views in apps/users: Import login_required, create profile_view function to render user profile template

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def login_view(request):
     return render(request, 'users/login.html')
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'users/profile.html', {'user': request.user})
