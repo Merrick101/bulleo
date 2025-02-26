7bad235956024e8284e26cd7fc43ec55c865457e
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 15:56:15 +0000

    Update views in apps/news: import Article model, modify homepage function to render articles, add article_detail function

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 
 def homepage(request):
-    return render(request, 'index.html')
+    # Get latest 10 articles
+    articles = Article.objects.all().order_by('-published_at')[:10]
+    return render(request, 'news/homepage.html', {'articles': articles})
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'news/article_detail.html', {'article': article})
