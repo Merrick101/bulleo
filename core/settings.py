a48f68e95e319c353a2184b12d5ab6a139d022cc
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 15:08:58 +0000

    Modify settings to finalize crispy-forms installation

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'apps.users',
     'apps.news',
     'apps.community',
@@ -132,8 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
