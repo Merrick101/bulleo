095083d2332f16b9695de603ecdab696272a319e
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-26 13:00:39 +0000

    Update news/admin to display images in admin panel by importing format_html, Article model and creating ArticleAdmin model

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Article
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_at', 'display_image')
+
+    def display_image(self, obj):
+        if obj.image_url:
+            return format_html(f'<img src="{obj.image_url}" width="100" height="60" />')
+        return "No Image"
+
+    display_image.short_description = "Article Image"
+
+
+admin.site.register(Article, ArticleAdmin)
