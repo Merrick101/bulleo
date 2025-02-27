8eef6e60485bd8dc05e51365ac0d63b6b712f524
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-27 14:04:53 +0000

    Modify users/views: Import login and authenticate from auth, import AuthenticationForm

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from .forms import ProfileForm
