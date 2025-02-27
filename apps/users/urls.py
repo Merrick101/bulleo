8c99b5c9f7eb777545eb86adfb3f66ffc30af933
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-27 14:27:25 +0000

    Create logout_view, add to url patterns, modify navbar to produce logout option when users are authenticated

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login_view, profile_view, signup_view
+from .views import login_view, profile_view, signup_view, logout_view
 
 app_name = "users"
 
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path('login/', login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
     path('profile/', profile_view, name="profile"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
@@ -49,3 +49,8 @@ def profile_view(request):
         form = ProfileForm(instance=profile)
 
     return render(request, 'users/profile.html', {'form': form, 'profile': profile})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")  # Redirect to homepage after logout
