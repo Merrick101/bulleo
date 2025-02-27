cd29f799133c9e0f4c571a97e477e64af144dc86
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-27 15:14:56 +0000

    Add password reset URLs to users/views, create registration template folder and html files

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from .views import login_view, logout_view, signup_view, profile_view
+from django.contrib.auth import views as auth_views
 
 app_name = "users"
 
 urlpatterns = [
+    # User Authentication URLs
     path('login/', login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("signup/", signup_view, name="signup"),
     path('profile/', profile_view, name="profile"),
+    # Password Reset URLs
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
