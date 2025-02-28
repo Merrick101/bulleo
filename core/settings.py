ed8c138aa6a700fcf279a4334dec059d301176ad
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-28 18:46:05 +0000

    Fix DEBUG evaluation for secure Heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False").lower() in ["true", "1"]
 
 ALLOWED_HOSTS = [
     'localhost',
