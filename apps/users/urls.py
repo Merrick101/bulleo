abe97198f502498758be4426a0fee33699316ede
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-03-02 19:40:30 +0000

    Add path to onboarding in urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, onboarding
 
 app_name = "users"
 
 urlpatterns = [
     # Allauth handles login/signup/logout
     path('profile/', profile_view, name="profile"),
+    path('onboarding/', onboarding, name="onboarding"),
 ]
