6bd6306ad457297f69a4f87287e60d61fa072e4a
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-03-05 09:56:33 +0000

    Add celery_result_backend to core settings, create task for verifying redis

--- apps/news/tasks.py
+++ apps/news/tasks.py
@@ -1,8 +1,15 @@
 import os
-from celery import Celery
+from celery import Celery, shared_task
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('bulleo')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@shared_task
+def debug_task():
+    message = "Debug task executed successfully."
+    print(message)
+    return message
